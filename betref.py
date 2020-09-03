09a82b0fdf1e734954c31a71a580d64addf6220c
R4MSOLO <r4msolo@protonmail.com>
2020-09-03 14:18:45 -0300

    Update betref.py

--- betref.py
+++ betref.py
@@ -192,7 +192,7 @@ class BetRef():
 					dork = "{}{}{}".format(engine,d,searchD)
 					self.filterResult(dork,se,d)
 				else:
-					dork = engine+' '.join(searchD)
+					dork = engine+''.join(searchD)
 					self.filterResult(dork,se,d)
 					break
 
